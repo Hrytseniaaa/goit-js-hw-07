54227b315a359624398371af4593e08446524bef
Hrytsenia Nazar <hrytseniaaa@gmail.com>
2022-08-03 00:11:40 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const refs = {
+
+ photo : document.querySelector('.gallery__image'),
+ gallery : document.querySelector('.gallery'),
+
+}    
+
+refs.gallery.addEventListener("click", onPhotoReturnUrl)
+
+function onPhotoReturnUrl(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    console.log(e.target.dataset.source);
+
+    
+}
+
+
+
+
+
 console.log(galleryItems);
+
+
+// import * as basicLightbox from ''
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()
