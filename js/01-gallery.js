0706ce731fa1f9f5272e938302f4af03ff32398f
Hrytsenia Nazar <hrytseniaaa@gmail.com>
2022-08-03 19:27:53 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,41 +1,66 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const refs = {
 
  photo : document.querySelector('.gallery__image'),
  gallery : document.querySelector('.gallery'),
 
-}    
+}  
 
-refs.gallery.addEventListener("click", onPhotoReturnUrl)
+
+const galleryItem = galleryItems.map(item => {
+    return (` <div class="gallery__item">
+  <a class="gallery__link" href= ${item.original} >
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>`)
+}).join("");
+
+
+ 
+
+
+refs.gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+refs.gallery.addEventListener("click", onPhotoReturnUrl,)
+
+
+let instance = null;
 
 function onPhotoReturnUrl(e) {
     e.preventDefault()
+
+
     if (e.target.nodeName !== 'IMG') {
         return
-    }
-    console.log(e.target.dataset.source);
-
+    }   instance = basicLightbox.create(`
+    <img src='${e.target.dataset.source}' width="800" height="600">
+`) 
+    instance.show()
     
+    console.log(e.target.dataset.source);
+    
+
 }
 
 
+const onModalClose = (e) => {
+    console.log(e.target);
+    if (e.key === 'Escape') {
+        instance.close()
+    }
+}
+ 
+
+refs.gallery.addEventListener('keydown', onModalClose)
+
+  
 
 
 
 console.log(galleryItems);
-
-
-// import * as basicLightbox from ''
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
-
-// instance.show()
