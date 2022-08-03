4fdff65b67877b279fefc45bf9af577b920acd32
Hrytsenia Nazar <hrytseniaaa@gmail.com>
2022-08-04 01:07:35 +0300

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+ const gallery = document.querySelector('.gallery')
+
+
+
+const galleryItem = galleryItems.map(item => {
+    
+    return (
+        `<a class="gallery__item"
+         href=${item.original}>
+  <img class="gallery__image" src=${item.preview} 
+  alt=${item.description} />
+</a>`)
+}).join("");
+
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+
+const shower = new SimpleLightbox(".gallery a", {
+    captionsData: 'alt',
+    captionsDelay: 250
+ })
+
+
 console.log(galleryItems);
